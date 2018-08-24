fdf5ade7ca8766d54fb7c5e27c8fda1501db4646
P1941269055 <david.vela.tirado@gmail.com>
2018-08-24 13:58:36 +0000

    dynamic behavior in the pie chart.

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,18 +4,46 @@ sap.ui.define([
 ], function (MessageToast, Controller) {
 	"use strict";
 	var data = {
-				'Sales': [{
-					"status": "Happy",
-					"value": "20"
-				}, {
-					"status": "Ok",
-					"value": "10"
-				}, {
-					"status": "Sad",
-					"value": "2"
-				}]
-			};
+		'DataToday': [{
+			"status": "Happy",
+			"value": "20"
+		}, {
+			"status": "Ok",
+			"value": "10"
+		}, {
+			"status": "Sad",
+			"value": "2"
+		}]
+	};
 	return Controller.extend("ussat.user_satisfaction_survey_sapui5.controller.View1", {
+
+		onPress: function (evt) {
+			// send the response to the server ... 
+			var oModel = new sap.ui.model.json.JSONModel();
+			var index;
+			if (evt.getSource().getId().includes("Happy") == true) {
+				index = 0;
+			} else if ( evt.getSource().getId().includes("OK") == true) {
+				index = 1;
+			}
+			else if ( evt.getSource().getId().includes("Sad") == true) {
+				index = 2;
+			};
+
+			if (index == undefined) {
+				MessageToast.show("Invalid Button");
+				return;
+			}
+
+			data["DataToday"][index]["value"] = parseInt(data["DataToday"][index]["value"]) + 1;
+
+			oModel.setData(data);
+			var oVizFrame = this.getView().byId("idpiechart");
+
+			MessageToast.show(evt.getSource().getId() + " Pressed");
+			oVizFrame.setModel(oModel);
+		},
+
 		onInit: function (evt) {
 			// example from http://www.saplearners.com/pie-chart-using-vizframe-sap-viz-ui5-controls-in-sapui5/
 			//      1.Get the id of the VizFrame		
@@ -23,7 +51,6 @@ sap.ui.define([
 
 			//      2.Create a JSON Model and set the data
 			var oModel = new sap.ui.model.json.JSONModel();
-
 			oModel.setData(data);
 
 			//      3. Create Viz dataset to feed to the data to the graph
@@ -37,7 +64,7 @@ sap.ui.define([
 					value: '{value}'
 				}],
 				data: {
-					path: "/Sales"
+					path: "/DataToday"
 				}
 			});
 			oVizFrame.setDataset(oDataset);
@@ -50,7 +77,7 @@ sap.ui.define([
 				},
 				plotArea: {
 					// colorPalette: d3.scale.category20().range(),
-					colorPalette:   ['yellow', "blue", 'red'], 
+					colorPalette: ['yellow', "blue", 'red'],
 					drawingEffect: "glossy"
 				}
 			});
@@ -68,13 +95,6 @@ sap.ui.define([
 			oVizFrame.addFeed(feedSize);
 			oVizFrame.addFeed(feedColor);
 
-		},
-
-		onPress: function (evt) {
-
-			// send the response to the server ... 
-			data
-			MessageToast.show(evt.getSource().getId() + " Pressed");
 		}
 
 	});
